a801cf193a083eb1a4f0cb18fa27ce5c5899af8c
Antonina <antoninakot95@gmail.com>
2023-03-27 00:20:31 +0300

    Style form template

---
+++ components/Form/Education.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="border-b border-gray-900/10 pb-12 w-[500px]">
+    <h2 class="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+
+
+    <div class="mt-10 grid grid-cols-2 gap-3">
+      <div>
+        <UiMainInput label="School" placeholder="School"/>
+      </div>
+
+      <div>
+        <UiMainInput label="Degree" placeholder="Degree"/>
+      </div>
+
+      <div class="flex gap-3 items-end">
+        <div>
+          <UiDatePicker label="Start & End date"/>
+        </div>
+        <div>
+          <UiDatePicker/>
+        </div>
+
+
+      </div>
+      <div>
+        <UiMainInput label="School" placeholder="School"/>
+      </div>
+      <div class="col-span-full">
+        <UiMainTextarea class="h-32" label="School" placeholder="School"/>
+      </div>
+
+
+    </div>
+  </div>
+  <!--  <div class="flex w-full">-->
+  <!--    <div class="columns-2 flex items-end">-->
+  <!--      <div class="w-full">-->
+  <!--        <UiMainInput label="School" placeholder="School"/>-->
+  <!--      </div>-->
+  <!--      <div class="w-full">-->
+  <!--        <UiMainInput label="Degree" placeholder="Degree"/>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--    <div class="columns-2 flex items-end">-->
+  <!--      <div class="columns-2 flex items-end gap-3">-->
+  <!--        <div class="w-full ">-->
+  <!--          <UiDatePicker label="Start & End Date"/>-->
+  <!--        </div>-->
+  <!--        <div class="w-full">-->
+  <!--          <UiDatePicker/>-->
+  <!--        </div>-->
+  <!--      </div>-->
+  <!--      <div class="w-full">-->
+  <!--        <UiSelectRegion label="Region" placeholder="Region"/>-->
+  <!--      </div>-->
+  <!--    </div>-->
+  <!--  </div>-->
+
+  <!--  <div class="columns-1">-->
+  <!--    <div class="w-full">-->
+  <!--      <UiMainTextarea label="Description"/>-->
+  <!--    </div>-->
+  <!--  </div>-->
+
+</template>
--- components/ui/DatePicker.vue
+++ components/ui/DatePicker.vue
@@ -1,47 +1,87 @@
 <template>
-  <label  class="block text-sm font-medium text-gray-500 transition-all
-     duration-75 ease-in-out group-focus-within:text-blue-400 capitalize pb-2" for="4">polkjhfg</label>
-  <VueDatePicker v-model="month" month-picker calendar-class-name="dp-custom-menu" locale="ru" />
-
-
+  <label :label="label" class="block text-sm font-medium text-gray-500 transition-all
+     duration-75 ease-in-out group-focus-within:text-blue-400 capitalize" >{{ label }}</label>
+  <VueDatePicker auto-apply v-model="month" calendar-class-name="dp-custom-menu" locale="ru" month-picker/>
 </template>
-
-<script setup>
-import { ref } from 'vue';
+<script lang="ts" setup>
+import {ref} from 'vue';
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
 
+withDefaults(defineProps<{
+  label?: string
+  placeholder?: string,
+  showTooltip:Boolean
+}>(), {
+  // defaults
+})
 const date = ref();
-
 const month = ref({
   month: new Date().getMonth(),
   year: new Date().getFullYear()
 });
+
 </script>
+
 <style>
+.dp__arrow_top, .dp__action_row {
+  display: none !important;
+}
+
+.dp__menu.dp__menu_index.dp__theme_light {
+  /*height: 250px !important;*/
+  border-radius: 12px;
+  overflow: hidden;
+}
+
+.dp__overlay_cell_pad {
+  font-size: 14px;
+  padding: 5px 0px;
+  border-radius: 20px;
+  text-transform: uppercase;
+}
+
+.dp__month_picker_header {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.dp__overlay_col {
+  padding: 3px;
+}
+
+.dp__overlay_container.dp__container_flex, .dp__month_year_row {
+  height: auto !important;
+}
+
+.dp__calendar div {
+  height: 200px !important;
+}
+
 .dp-custom-menu {
   box-shadow: 0 0 6px #1976d2;
   background: rebeccapurple;
 }
+
 .dp__theme_light {
-  --dp-background-color:#f9fafb;
+  --dp-background-color: #f9fafb;
   --dp-text-color: #212121;
-
-  --dp-primary-color:#00000;
-
-  --dp-border-color:transparent;
-
+  --dp-primary-color: #00000;
+  --dp-border-color: transparent;
   --dp-border-color-hover: transparent;
 
 }
+
 .dp__input {
   border-bottom: 2px solid transparent;
-border-radius: 0;
+  border-radius: 0;
 }
+
 .dp__input_focus {
   background-color: #FFFFFF !important;
   border-bottom: 2px solid var(--color-main) !important;
 }
+
 .dp__overlay_cell_active {
   background: var(--color-main);
 }
